20ffacdc5844b81e9661566159d9615389daba5f
Geoffrey McRae <geoff@hostfission.com>
2025-12-10 13:33:40 +1100

    fix build on older version of libconfig without const args

--- src/config.c
+++ src/config.c
@@ -70,10 +70,10 @@ static bool rr_config_read_string_array(const config_setting_t *array, const cha
   return true;
 }
 
-static bool rr_config_read_list_filter(const config_setting_t *filter, ConfigFilter *out)
+static bool rr_config_read_list_filter(config_setting_t *filter, ConfigFilter *out)
 {
-  const config_setting_t *match  = config_setting_lookup(filter, "match" );
-  const config_setting_t *ignore = config_setting_lookup(filter, "ignore");
+  config_setting_t *match  = config_setting_lookup(filter, "match" );
+  config_setting_t *ignore = config_setting_lookup(filter, "ignore");
 
   if (match && !rr_config_read_string_array(match, &out->match))
   {
@@ -177,8 +177,8 @@ bool rr_config_init(void)
     const config_setting_t *s = config_setting_get_elem(lists, i);
 
     list->name = config_setting_name(s);
-    const config_setting_t *include  = config_setting_lookup(s, "include" );
-    const config_setting_t *exclude  = config_setting_lookup(s, "exclude" );
+    config_setting_t *include  = config_setting_lookup(s, "include" );
+    config_setting_t *exclude  = config_setting_lookup(s, "exclude" );
 
     if (include && !rr_config_read_string_array(include, &list->include))
     {
@@ -193,7 +193,7 @@ bool rr_config_init(void)
     }
 
     #define LOAD_FILTER(x) \
-      const config_setting_t *x = config_setting_lookup(s, #x); \
+      config_setting_t *x = config_setting_lookup(s, #x); \
       if (x && !rr_config_read_list_filter(x, &list->x)) \
       { \
         LOG_ERROR("lists.%s." #x " failed to load", list->name); \
